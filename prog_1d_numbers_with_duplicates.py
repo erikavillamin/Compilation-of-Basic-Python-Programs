2930bfea7af57147d31f17d721ba4293b03a6fbf
erikavillamin <erikamaevillamin@gmail.com>
2025-03-13 03:45:29 +0800

    Update prog_1d_numbers_with_duplicates.py

--- prog_1d_numbers_with_duplicates.py
+++ prog_1d_numbers_with_duplicates.py
@@ -1,5 +1,6 @@
 #Display the numbers that have duplicates
 numbers = [float(input(f"Enter number {num + 1}: ")) for num in range(10)]
+
 duplicates = list(set([num for num in numbers if numbers.count(num) > 1]))
 
 if duplicates:
