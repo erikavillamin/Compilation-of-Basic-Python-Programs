1b8bd9af15e998e77f490c7797207b2581c8967f
erikavillamin <erikamaevillamin@gmail.com>
2025-03-13 04:31:11 +0800

    Update prog_3d_highest_number.py

---
+++ prog_3d_highest_number.py
@@ -0,0 +1,18 @@
+#Display the highest number
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        print("Invalid")
+        break
+
+
+if numbers:
+    print("The highest number is", max(numbers), end=".")
+else:
+    print("Enter another input.")
+
+
